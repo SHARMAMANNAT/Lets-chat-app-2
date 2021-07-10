576898299bf236990e3ca82b12c87230fa3086e3
SHARMAMANNAT <83449796+SHARMAMANNAT@users.noreply.github.com>
2021-07-10 20:49:02 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDN2RGiwyL0lrYOg9o___EeqBjZR6qEmL0",
+    authDomain: "lets-chat-web-app-58c79.firebaseapp.com",
+    projectId: "lets-chat-web-app-58c79",
+    storageBucket: "lets-chat-web-app-58c79.appspot.com",
+    messagingSenderId: "1092819675270",
+    appId: "1:1092819675270:web:560b335d55ac7e1ff34907"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
